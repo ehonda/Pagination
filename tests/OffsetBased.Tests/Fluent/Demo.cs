d598a0f286066677d7983a37331e23916be572d8
Dennis Renz <dennis@r-renz.de>
2024-09-01 23:49:42 +0200

    Add two problems to demo

--- tests/OffsetBased.Tests/Fluent/Demo.cs
+++ tests/OffsetBased.Tests/Fluent/Demo.cs
@@ -26,4 +26,39 @@ public static class Demo
             .WithItemExtraction(_ => null!)
             .GetAllItemsAsync();
     }
+
+    public static void Problem_direct_handler_usage()
+    {
+        var httpClient = new HttpClient();
+
+        var handler = httpClient
+            .WithPagination()
+            .OffsetBased<Numbers, int>()
+            .Build();
+
+        // Now we can't do this as it produces the following error message:
+        //     Method 'GetAllItemsAsync' has 2 parameter(s) but is invoked with 0 argument(s)
+        // var numbers = handler.GetAllItemsAsync();
+        
+        // Instead we have to do this which forces us to specify the HttpClient again
+        var numbers = handler.GetAllItemsAsync(httpClient);
+
+        // And we can even do this which can lead to bugs if the first HttpClient is configured differently
+        var anotherHttpClient = new HttpClient();
+        var anotherNumbers = handler.GetAllItemsAsync(anotherHttpClient);
+    }
+
+    public static void Problem_Multiple_WithHttpClient_calls()
+    {
+        var httpClient = new HttpClient();
+        var anotherHttpClient = new HttpClient();
+
+        // This is confusing and should not be possible
+        var numbers = httpClient
+            .WithPagination()
+            .OffsetBased<Numbers, int>()
+            .WithHttpClient(httpClient)
+            .WithHttpClient(anotherHttpClient)
+            .GetAllItemsAsync();
+    }
 }
