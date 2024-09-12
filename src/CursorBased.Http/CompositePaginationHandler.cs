d31441f4d58a36b323e0f0326d96fd80db7bdd5d
Dennis Renz <dennis@r-renz.de>
2024-09-12 23:48:00 +0200

    WIP: CursorBased.Http

---
+++ src/CursorBased.Http/CompositePaginationHandler.cs
@@ -0,0 +1,16 @@
+﻿using CursorBased.Composite;
+using Sequential.Composite;
+
+namespace CursorBased.Http;
+
+public abstract class CompositePaginationHandler<TTransformedPage, TCursor, TItem>
+    : CompositePaginationHandler<HttpResponseMessage, TTransformedPage, TCursor, TItem>
+{
+    protected CompositePaginationHandler(
+        PageRetriever<TTransformedPage, TCursor> pageRetriever,
+        IPaginationContextExtractor<HttpResponseMessage, PaginationContext<TTransformedPage, TCursor>> contextExtractor,
+        IItemExtractor<PaginationContext<TTransformedPage, TCursor>, TItem> itemExtractor)
+        : base(pageRetriever, contextExtractor, itemExtractor)
+    {
+    }
+}
---
+++ src/CursorBased.Http/PageRetriever.cs
@@ -0,0 +1,18 @@
+﻿using Sequential.Composite;
+
+namespace CursorBased.Http;
+
+public abstract class PageRetriever<TTransformedPage, TCursor>
+    : IPageRetriever<PaginationContext<TTransformedPage, TCursor>, HttpResponseMessage>
+{
+    protected HttpClient HttpClient { get; }
+    
+    public PageRetriever(HttpClient httpClient)
+    {
+        HttpClient = httpClient;
+    }
+
+    public abstract Task<HttpResponseMessage> GetAsync(
+        PaginationContext<TTransformedPage, TCursor>? context,
+        CancellationToken cancellationToken = default);
+}
